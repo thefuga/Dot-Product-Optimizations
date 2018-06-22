8cb956b9d3388a00e8114e32f96218ae84ed5db2
Erick Costa Fuga <erick_fuga@hotmail.com>
2018-06-22 18:30:40 -0300

    Different vectors for parallel and sequential versions

--- main.c
+++ main.c
@@ -13,21 +13,13 @@
 int main(){
 
     //Variáveis p/ cálculo
-	long long int vector_length = 10;
-    long long int result;
+	long long unsigned int vector_length = 100000000;
     double start, end;
 
-    //Arquivos para resultados
-    FILE *loop_file = fopen("loop.csv","wb");
-    FILE *pairs_file = fopen("pairs.csv","wb");
-    FILE *simd_file = fopen("simd.csv","wb");
-    FILE *parallel_file = fopen("parallel.csv","wb");
-    FILE *parallel_simd_file = fopen("parallel_simd.csv","wb");
-
     //Vetores utilizados
-	int *vector_a = _mm_malloc(vector_length * sizeof(double), 64);
+	int *vector_a = malloc(vector_length * sizeof(int));
 	assert(vector_a != NULL);
-	int *vector_b = _mm_malloc(vector_length * sizeof(double), 64);
+	int *vector_b = malloc(vector_length * sizeof(int));
 	assert(vector_b != NULL);
 
     //Preenchimento dos vetores
@@ -39,71 +31,88 @@ int main(){
     //--------------------------------------------------------------------------
 
     //Loop
+	FILE *loop_file = fopen("loop.csv","wb");
     fputs("sep=,\nIteração,Tempo\n", loop_file);
-    for(long long int i = 0; i < 101; ++i){
+    for(long long unsigned int i = 0; i < 100; ++i){
         start = omp_get_wtime();
-    	result = dot_product_loop(vector_a, vector_b, vector_length);
+    	dot_product_loop(vector_a, vector_b, vector_length);
     	end = omp_get_wtime();
-        fprintf(loop_file, "%ld, %f\n", i, end-start);
-        //printf("Loop: \nResult: %ld\nTime: %f\n", result, end - start);
+        fprintf(loop_file, "%I64u, %f\n", i, end-start);
     }
-
+	fclose(loop_file);
 
     //Pairs
+	FILE *pairs_file = fopen("pairs.csv","wb");
     fputs("sep=,\nIteração,Tempo\n", pairs_file);
-    for(long long int i = 0; i < 101; ++i){
+    for(long long int i = 0; i < 100; ++i){
         start = omp_get_wtime();
-    	result = dot_product_pairs(vector_a, vector_b, vector_length);
+    	dot_product_pairs(vector_a, vector_b, vector_length);
     	end = omp_get_wtime();
-        fprintf(pairs_file, "%ld, %f\n", i, end-start);
-        //printf("Pairs: \nResult: %ld\nTime: %f\n", result, end - start);
+        fprintf(pairs_file, "%I64u, %f\n", i, end-start);
     }
-
+	fclose(pairs_file);
 
     //Recursion
-    /*start = omp_get_wtime();
-	result = dot_product_recursion(vector_a, vector_b, vector_length-1);
-	end = omp_get_wtime();
-    printf("Recursion: \nResult: %f\nTime: %f\n", result, end - start);*/
+	FILE *recursion_file = fopen("recursion.csv","wb");
+	fputs("sep=,\nIteração,Tempo\n", recursion_file);
+    for(long long int i = 0; i < 100; ++i){
+        start = omp_get_wtime();
+    	dot_product_pairs(vector_a, vector_b, vector_length);
+    	end = omp_get_wtime();
+        fprintf(recursion_file, "%I64u, %f\n", i, end-start);
+    }
+	fclose(recursion_file);
+
+	free(vector_a);
+	free(vector_b);
+
+	//Vetores utilizados com alinhamento.
+	int *vector_a_alligned = _mm_malloc(vector_length * sizeof(int), 64);
+	assert(vector_a != NULL);
+	int *vector_b_alligned = _mm_malloc(vector_length * sizeof(int), 64);
+	assert(vector_b != NULL);
+
+	//Preenchimento dos vetores
+	for(long long int i = 0; i < vector_length; ++i){
+		vector_a_alligned[i] = 1;
+		vector_b_alligned[i] = 2;
+	}
 
     //SIMD
+	FILE *simd_file = fopen("simd.csv","wb");
     fputs("sep=,\nIteração,Tempo\n", simd_file);
-    for(long long int i = 0; i < 101; ++i){
+    for(long long int i = 0; i < 100; ++i){
         start = omp_get_wtime();
-    	result = dot_product_simd(vector_a, vector_b, vector_length);
+    	dot_product_simd(vector_a_alligned, vector_b_alligned, vector_length);
     	end = omp_get_wtime();
-        fprintf(simd_file, "%ld, %f\n", i, end-start);
-        //printf("SIMD: \nResult: %ld\nTime: %f\n", result, end - start);
+        fprintf(simd_file, "%I64u, %f\n", i, end-start);
     }
+    fclose(simd_file);
 
     //parallel
+    FILE *parallel_file = fopen("parallel.csv","wb");
     fputs("sep=,\nIteração,Tempo\n", parallel_file);
-    for(long long int i = 0; i < 101; ++i){
+    for(long long int i = 0; i < 100; ++i){
         start = omp_get_wtime();
-    	result = dot_product_parallel(vector_a, vector_b, vector_length);
+    	dot_product_parallel(vector_a_alligned, vector_b_alligned, vector_length);
     	end = omp_get_wtime();
-        fprintf(parallel_file, "%ld, %f\n", i, end-start);
-        //printf("Parallel: \nResult: %ld\nTime: %f\n", result, end - start);
+        fprintf(parallel_file, "%I64u, %f\n", i, end-start);
     }
+	fclose(parallel_file);
 
     //SIMD parallel
+	FILE *parallel_simd_file = fopen("parallel_simd.csv","wb");
     fputs("sep=,\nIteração,Tempo\n", parallel_simd_file);
-    for(long long int i = 0; i < 101; ++i){
+    for(long long int i = 0; i < 100; ++i){
         start = omp_get_wtime();
-    	result = dot_product_parallel_simd(vector_a, vector_b, vector_length);
+    	dot_product_parallel_simd(vector_a_alligned, vector_b_alligned, vector_length);
     	end = omp_get_wtime();
-        fprintf(parallel_simd_file, "%ld, %f\n", i, end-start);
-        //printf("Parallel SIMD: \nResult: %ld\nTime: %f\n", result, end - start);
+        fprintf(parallel_simd_file, "%I64u, %f\n", i, end-start);
     }
+	fclose(parallel_simd_file);
 
     _mm_free(vector_a);
 	_mm_free(vector_b);
-
-    fclose(loop_file);
-    fclose(pairs_file);
-    fclose(simd_file);
-    fclose(parallel_file);
-    fclose(parallel_simd_file);
 
 	return 0;
 }
